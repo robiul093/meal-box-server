006a6f48a7969a5767dd4973425df13e72754bf6
Robiul@Hasan <robihasan093@gmail.com>
2025-02-28 21:56:29 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Meal Box is running!');
+});
+
+export default app;
